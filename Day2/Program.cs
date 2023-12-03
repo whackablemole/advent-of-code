54a1e93f4f3819a5dff000d3db6d80471a560d5c
WhackableMole <whackablemole@gmail.com>
2023-12-03 21:13:44 +0000

    Part 1 Day 2 complete.

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,25 +1,78 @@
 ﻿
-// Read input.txt into a string array
 System.IO.StreamReader file = new System.IO.StreamReader("input.txt");
 string input = file.ReadToEnd();
 file.Close();
 
 string[] lines = input.Split('\n');
 List<Game> games = new List<Game>();
+int max_red = 12;
+int max_green = 13;
+int max_blue = 14;
+
+int idSum = 0;
 
 foreach (string line in lines) {
 	Game game = new Game();
 
-	// Find the Id of the game
-	game.Id = int.Parse(line.Split(":")[0].Replace("Game ", ""));
-	Console.WriteLine(game.Id);
+	string[] data = line.Split(":");
+
+	game.Id = int.Parse(data[0].Replace("Game ", ""));
+
+	string[] matches = data[1].Split(";");
+	foreach (string m in matches)
+	{
+		Match match = new Match();
+		List<string> match_data = m.Split(",").ToList<string>();
+		for (var i = 0; i < match_data.Count; i++)
+		{
+			string d = match_data[i];
+			if (d.Contains(" red"))
+			{
+				match.Red = int.Parse(d.Replace(" red", ""));
+			}
+			else if (d.Contains(" blue"))
+			{
+				match.Blue = int.Parse(d.Replace(" blue", ""));
+			}
+			else if (d.Contains(" green"))
+			{
+				match.Green = int.Parse(d.Replace(" green", ""));
+			}
+		}
+		game.Matches.Add(match);
+	}
+	games.Add(game);
 }
+
+List<Game> possible = new List<Game>();
+foreach (Game g in games)
+{
+    bool isPossible = true;
+    foreach (Match m in g.Matches)
+	{
+        if (m.Red > max_red || m.Blue > max_blue || m.Green > max_green)
+		{
+            isPossible = false;
+            break;
+        }
+    }
+    if (isPossible)
+	{
+        possible.Add(g);
+    }
+}
+
+foreach (Game g in possible)
+{
+	idSum += g.Id;
+	Console.WriteLine(g.Id);
+}
+
+Console.WriteLine(idSum);
 
 class Game {
 	public int Id { get; set; }
-	public Match Match1 { get; set; } = new Match();
-	public Match Match2 { get; set; } = new Match();
-	public Match Match3 { get; set; } = new Match();
+	public List<Match> Matches { get; set; } = new List<Match>();
 }
 
 class Match {
