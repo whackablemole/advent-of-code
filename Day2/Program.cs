fe06b3ffe67d612a342040fc6081e3850a5c34af
Mark Townsend <whackablemole@gmail.com>
2023-12-03 20:39:13 +0000

    Started day 2.

---
+++ Day2/Program.cs
@@ -0,0 +1,29 @@
+﻿
+// Read input.txt into a string array
+System.IO.StreamReader file = new System.IO.StreamReader("input.txt");
+string input = file.ReadToEnd();
+file.Close();
+
+string[] lines = input.Split('\n');
+List<Game> games = new List<Game>();
+
+foreach (string line in lines) {
+	Game game = new Game();
+
+	// Find the Id of the game
+	game.Id = int.Parse(line.Split(":")[0].Replace("Game ", ""));
+	Console.WriteLine(game.Id);
+}
+
+class Game {
+	public int Id { get; set; }
+	public Match Match1 { get; set; } = new Match();
+	public Match Match2 { get; set; } = new Match();
+	public Match Match3 { get; set; } = new Match();
+}
+
+class Match {
+	public int Red { get; set; }
+	public int Blue { get; set; }
+	public int Green { get; set; }
+}
